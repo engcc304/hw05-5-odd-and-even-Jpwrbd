e89f77d890307f7513dbd072aa07dc9ead134e81
Jpwrbd <jpwrbd456@gmail.com>
2023-08-04 02:59:44 +0700

    up got

--- Homework/HW05/hw05-5-odd-even.cpp
+++ Homework/HW05/hw05-5-odd-even.cpp
@@ -1,6 +1,6 @@
 /*
     จงรับตัวเลขจากผู้ใช้ และให้แสดงผลลัพธ์ว่าเป็นเลขคู่หรือเลขคี่
-    
+
     Test case:
         Input :
             15
@@ -16,3 +16,18 @@
         -22 is an even integer
 
 */
+#include <stdio.h>
+int main()
+{
+    int N = 0;
+    printf("Input : ");
+    scanf("%d", &N);
+    printf("Output: ");
+    if (N % 2 == 0)
+    {
+        printf("%d is an odd integer", N);
+    }
+    else
+        printf("%d is an odd integer", N);
+    return 0;
+}
